54ae9a0ebd8c5e86cfbe3db7dff85cf5a0c599f6
abraham polishchuk <apolishc@gmail.com>
2017-03-11 19:25:37 +0000

    a bit of wiring to take input

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,12 @@ module Lib
     ( someFunc
     ) where
 
+generateMultiplyAndPrint :: Int -> IO ()
+generateMultiplyAndPrint 0 = putStrLn "foo"
+generateMultiplyAndPrint _ = putStrLn "bar"
+
+getInput :: IO String
+getInput = putStrLn "Please enter the number of primes you with to generate and multiply" >> getLine
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = getInput >>= \number -> generateMultiplyAndPrint (read number :: Int)
